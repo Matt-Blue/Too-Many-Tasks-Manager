938c36a0fef9eccf64cbdb8711c921608e2ef0f0
Michael Pereira III <migrepereira@gmail.com>
2018-04-17 21:31:19 -0400

    Added test email

--- routes/web.php
+++ routes/web.php
@@ -43,9 +43,14 @@ Route::post ('/preferences/post', 'TaskController@doEditPreferences')->name('do_
 
 
 
+// Test email
 
 
-
-
+Route::get('send_test_email', function(){
+    Mail::raw('Sending emails with Mailgun and Laravel is easy!', function($message)
+    {
+        $message->to('migrepereira@gmail.com');
+    });
+});
 
 
